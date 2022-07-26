ee391594119b3d05a1d05fb291d667bf372183cd
Faisal Musa <76185989+faisalm36@users.noreply.github.com>
2022-07-26 14:42:31 -0400

    Add files via upload

---
+++ chat_app/lib/screens/homePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(child: Text("Chat")),
+      ), 
+      return Scaffold(
+      body: ChatPage(),
+      bottomNavigationBar: BottomNavigationBar();
+  }
+}
+
